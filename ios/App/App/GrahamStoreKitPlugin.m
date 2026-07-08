edcb315d2c40b2429da5cc62e60e06e943a4e944
Anthony Edwards <anthonyedwards@Anthonys-MacBook-Air-18.local>
2026-07-08 15:35:39 -0400

    Use native StoreKit for Graham IAP checkout

---
+++ ios/App/App/GrahamStoreKitPlugin.m
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+#import <Capacitor/Capacitor.h>
+
+CAP_PLUGIN(GrahamStoreKitPlugin, "GrahamStoreKit",
+           CAP_PLUGIN_METHOD(products, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(purchase, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(restore, CAPPluginReturnPromise);
+)
